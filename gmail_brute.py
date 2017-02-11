df5bfeb0164caba79b45972f32461ad0dc535496
Ahmed Safa <ahmedsafa407@gmail.com>
2017-02-11 11:53:37 +0600

    Update gmail_brute.py

--- gmail_brute.py
+++ gmail_brute.py
@@ -3,29 +3,29 @@ import datetime
 import sys
 import platform
 
-print "**********Gmail Login BruteForcer**********"
-
-print "**************************************************"
-
-print "**********Authon : Ahmed Safa***********************"
-
-
-print "**************************************************"
-
-print "***************************************"
-
-
-
+print "\n\n"
+print "********************************************************"
+print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
+print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
+print "|++++++++++++++++++++\033[1;36mGmail Brute Force\033[0m++++++++++++++++++|"
+print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
+print "|+++++++++++++++++Author : \033[1;32mAhmed Safa\033[0m++++++++++++++++++|"
+print "|+++++++++++++++facebook/\033[32mahmedsafa404\033[0m++++++++++++++++++|"
 
 #Info
 os = platform.system()
 sys = platform.release()
-print ("Operating System : " +os)
-print sys
-print("+++++++++++++++++++++++++++++++++++++")
-current_time = datetime.datetime.now()
-print current_time
-print ("********************************")
+print "|+++++++++++++++++Operating System :" +os +"++++++++++++++|"
+
+print"|++++++++++++Released :"+sys+"++++++++++++++++|"
+time = datetime.datetime.now()
+print "|+++++++\033[34mDate & Time\033[0m :"+str(time)+"++++++++|"
+print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
+print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
+print "********************************************************"
+
+print "\n"
+
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.ehlo()
